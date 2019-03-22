d97f372a8f5dbf119c19c811321a332ce08cc256
Adam <46858866+weenie-walker-games@users.noreply.github.com>
2019-03-21 23:36:40 -0400

    refactor: Adjust CharControl script to better match best practices
    - Update CharControl script to make variables private
    
    - Create variable for the Animation component to cache that for use in multiple locations
    
    - Create variable for the speed values that were hard-coded during the training

--- GTA_Style_Clone/Assets/Script/Characters/CharControl.cs
+++ GTA_Style_Clone/Assets/Script/Characters/CharControl.cs
@@ -5,28 +5,44 @@ using UnityEngine;
 public class CharControl : MonoBehaviour
 {
 
+    //public variable (might be refactored depending on how they are used in later tutorial videos)
     public GameObject thePlayer;
-    public bool isRunning;
-    public float horizontalMove;
-    public float verticalMove;
 
+
+    //private but viewable in Inspector window
+    [SerializeField] private float _horizontalSpeed = 150;
+    [SerializeField] private float _verticalSpeed = 8;
+
+
+    //hidden from Inspector window
+    private Animation _anim;
+    private bool _isRunning;
+    private float _horizontalMove;
+    private float _verticalMove;
+
+
+    private void Start()
+    {
+        _anim = thePlayer.GetComponent<Animation>();
+
+    }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
         {
-            thePlayer.GetComponent<Animation>().Play("Run");
-            horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * 150;    //spinning around on y
-            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * 8;          //forwards and backwards on zed
-            isRunning = true;
-            transform.Rotate(0, horizontalMove, 0);
-            transform.Translate(0, 0, verticalMove);
+            _anim.Play("Run");
+            _horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * _horizontalSpeed;    //spinning around on y
+            _verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * _verticalSpeed;          //forwards and backwards on zed
+            _isRunning = true;
+            transform.Rotate(0, _horizontalMove, 0);
+            transform.Translate(0, 0, _verticalMove);
         }
         else
         {
-            thePlayer.GetComponent<Animation>().Play("Idle");
-            isRunning = false;
+            _anim.Play("Idle");
+            _isRunning = false;
         }
     }
 }
