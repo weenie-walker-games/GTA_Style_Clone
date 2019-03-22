e3fb9580000898ef5bebea2d23f37bb4dd694117
Adam <46858866+weenie-walker-games@users.noreply.github.com>
2019-03-21 23:16:28 -0400

    feat: Add beginnings of a character controller script

--- GTA_Style_Clone/Assets/Script/Characters/CharControl.cs
+++ GTA_Style_Clone/Assets/Script/Characters/CharControl.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class CharControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public GameObject thePlayer;
+    public bool isRunning;
+    public float horizontalMove;
+    public float verticalMove;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            thePlayer.GetComponent<Animation>().Play("Run");
+            horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * 150;    //spinning around on y
+            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * 8;          //forwards and backwards on zed
+            isRunning = true;
+            transform.Rotate(0, horizontalMove, 0);
+            transform.Translate(0, 0, verticalMove);
+        }
     }
 }
